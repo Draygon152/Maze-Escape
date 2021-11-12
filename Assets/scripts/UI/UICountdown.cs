24143de91958695de43a37f3b84b1886931f849b
Ryan Bascos <ryanjbascos@gmail.com>
2021-11-11 19:59:11 -0800

    Timer Code
    timer begins once player loads into maze, loss scene activates once timer hits 0, loss screen returns to main menu after 5 seconds

--- Assets/scripts/UI/UICountdown.cs
+++ Assets/scripts/UI/UICountdown.cs
@@ -1,23 +1,31 @@
 using UnityEngine;
 using System.Threading.Tasks;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UICountdown : MonoBehaviour
 {
     public int timeRemaining;
+    public GameObject player;
+    CharacterController playerCC;
 
+    bool hasTimerStarted;
     float nextSecondEndTime;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerCC = player.GetComponent<CharacterController>();
+        hasTimerStarted = false;
         timeRemaining = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("k"))
+        // once player moves or looks around and timer hasn't started yet, start timer
+        // without !hasTimerStarted, BeginCountdown would be called every time player moves after not moving
+        if (playerCC.velocity.magnitude > 0 && !hasTimerStarted)
         {
             BeginCountdown();
         }
@@ -25,20 +33,27 @@ public class UICountdown : MonoBehaviour
 
     async void BeginCountdown()
     {
+        hasTimerStarted = true;
+
+        // wait until the task timerTick has finished before running timerTick again
         for (int i = 0; i < 60; i++)
         {
             await timerTick();
         }
+        SceneManager.LoadScene("LossScene");
     }
 
     async Task timerTick()
     {
+        // task is not complete until 1 second has passed
         nextSecondEndTime = Time.time + 1;
         while (Time.time < nextSecondEndTime)
         {
             await Task.Yield();
         }
 
+        // update timeRemaining and UI timer
+        Debug.Log(timeRemaining);
         timeRemaining -= 1;
         gameObject.GetComponent<TextMeshProUGUI>().text = $"Time Remaining: {timeRemaining}";
     }
