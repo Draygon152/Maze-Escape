c435bff484f02dc83eb674573f23de2ab93d3cda
Draygon <Raiden152@yahoo.com>
2021-11-30 15:24:54 -0800

    Revert "Merge branch 'master' of https://github.com/Draygon152/Scaffold-Project-Group-10-"
    This reverts commit a465fe8e9aa1e57cffeda034de436a34e289dd93, reversing
    changes made to 630d2ce673e80a929733d7ed369f0ef9bef256e1.

--- Assets/scripts/UI/MazeSelection.cs
+++ Assets/scripts/UI/MazeSelection.cs
@@ -1,23 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MazeSelection : MonoBehaviour
-{
-    public void RandomGenerate()
-    {
+public class MazeSelection : MonoBehaviour {
+    public void RandomGenerate() {
         SceneManager.LoadScene("ProceduralMaze");
-        Time.timeScale = 1f;
     }
 
 
-    public void prefab()
-    {
+    public void prefab() {
         SceneManager.LoadScene("PrefabMaze");
-        Time.timeScale = 1f;
     }
 
 
-    public void QuitGame()
-    {
+    public void QuitGame() {
         Debug.Log("QUIT GAME!");
         Application.Quit();
     }
